03a5cea721cc058b3d04e4c1457d227d175d312f
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2020-07-03 10:18:29 +0100

    Sort client id mappings before setting the result futures

--- node/src/main/kotlin/net/corda/node/services/statemachine/SingleThreadedStateMachineManager.kt
+++ node/src/main/kotlin/net/corda/node/services/statemachine/SingleThreadedStateMachineManager.kt
@@ -942,12 +942,12 @@ class SingleThreadedStateMachineManager(
         require(lastState.isRemoved) { "Flow must be in removable state before removal" }
         require(lastState.checkpoint.checkpointState.subFlowStack.size == 1) { "Checkpointed stack must be empty" }
         require(flow.fiber.id !in sessionToFlow.values) { "Flow fibre must not be needed by an existing session" }
-        flow.resultFuture.set(removalReason.flowReturnValue)
         flow.fiber.clientID?.let {
             val oldClientIdFlowStatus = clientIDsToFlowIds[it]
             require (oldClientIdFlowStatus != null && oldClientIdFlowStatus is FlowWithClientIdStatus.Active)
             clientIDsToFlowIds[it] = FlowWithClientIdStatus.Removed(flow.fiber.id, FlowWithClientIdStatus.Removed.Status.SUCCEEDED)
         }
+        flow.resultFuture.set(removalReason.flowReturnValue)
         lastState.flowLogic.progressTracker?.currentStep = ProgressTracker.DONE
         changesPublisher.onNext(StateMachineManager.Change.Removed(lastState.flowLogic, Try.Success(removalReason.flowReturnValue)))
     }
@@ -958,16 +958,16 @@ class SingleThreadedStateMachineManager(
             lastState: StateMachineState
     ) {
         drainFlowEventQueue(flow)
-        val flowError = removalReason.flowErrors[0] // TODO what to do with several?
-        val exception = flowError.exception
-        (exception as? FlowException)?.originalErrorId = flowError.errorId
-        flow.resultFuture.setException(exception)
-        lastState.flowLogic.progressTracker?.endWithError(exception)
         flow.fiber.clientID?.let {
             val oldClientIdFlowStatus = clientIDsToFlowIds[it]
             require (oldClientIdFlowStatus != null && oldClientIdFlowStatus is FlowWithClientIdStatus.Active)
             clientIDsToFlowIds[it] = FlowWithClientIdStatus.Removed(flow.fiber.id, FlowWithClientIdStatus.Removed.Status.FAILED)
         }
+        val flowError = removalReason.flowErrors[0] // TODO what to do with several?
+        val exception = flowError.exception
+        (exception as? FlowException)?.originalErrorId = flowError.errorId
+        flow.resultFuture.setException(exception)
+        lastState.flowLogic.progressTracker?.endWithError(exception)
         changesPublisher.onNext(StateMachineManager.Change.Removed(lastState.flowLogic, Try.Failure<Nothing>(exception)))
     }
 
